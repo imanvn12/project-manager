7d6706ab1695920e04a720d5d785e80be1a1adef
imanvn12 <vakilianiman9@gmail.com>
2023-08-26 19:35:10 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const Application = require('./app/server')
-const DB_url = 'mongodb://127.0.0.1:27017/passport-js'
+const DB_url = 'mongodb://127.0.0.1:27017/project-managerDB'
 new Application(4000, DB_url)
