e8276243f0f0fa3add5a814b47fa9ffdebbf547c
imanvn12 <vakilianiman9@gmail.com>
2023-08-26 20:20:05 +0330

    edit

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 module.exports = class Application {
-    #express = require('express')
+    #express = require('express');
     #app = this.#express();
     constructor(PORT, DB_url) {
         this.configDatabase(DB_url),
@@ -9,41 +9,40 @@ module.exports = class Application {
         this.errorHandler()
     }
     configApplication() {
-        const path = require('path')
-        this.#app.use(this.#express.json())
-        this.#app.use(this.#express.urlencoded({ extended: true }))
-        this.#app.use(this.#express.static(path.join(__dirname, '..', 'public')))
+        const path = require('path');
+        this.#app.use(this.#express.json());
+        this.#app.use(this.#express.urlencoded({ extended: true }));
+        this.#app.use(this.#express.static(path.join(__dirname, '..', 'public')));
     }
     createServerr(PORT) {
         const http = require('http');
-        const server = http.createServer();
+        const server = http.createServer(this.#app);
         server.listen(PORT, () => { console.log(`server run on port : http://localhost:${PORT}`); });
     }
     configDatabase(DB_url) {
-        const mongoose = require("mongoose")
+        const mongoose = require("mongoose");
         mongoose.connect(DB_url)
     }
     errorHandler() {
         this.#app.use((req, res, next) => {
-            res.status(404).json({
+            return res.status(404).json({
                 status: 404,
                 message: 'not found'
             })
-
-            this.#app((err, req, res, next) => {
-                const status = err?.statusCode || 500
-                const message = err?.message || 'internal error'
-                res.status(status).json({
-                    status: status,
-                    message: message
-                })
+        });
+        this.#app.use((err, req, res, next) => {
+            const status = err?.statusCode || 500;
+            const message = err?.message || 'internal error';
+            return res.status(status).json({
+                status: status,
+                message: message
             })
         })
     }
     createRoutes() {
         this.#app.get('/', (req, res, next) => {
             try {
-                res.status(200).json({ message: 'this is express server' })
+                return res.status(200).json({ message: 'this is express server' })
             } catch (error) {
                 next(error)
             }
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const Application = require('./app/server');
 const DB_url = 'mongodb://127.0.0.1:27017/pm';
-new Application(3000, DB_url)
+new Application(4000, DB_url)
